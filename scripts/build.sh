03b7d34ac4cda68dc998de584dd769c945d21bfe
Linus Fischer <88381277+LeiCraft@users.noreply.github.com>
2026-01-26 10:36:45 +0100

    update

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# check if version argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+rm -f debian/changelog
+
+# Create a temporary changelog with the version you want
+cat > debian/changelog <<EOF
+leios-branding-meta-files ($1) stable; urgency=medium
+
+  * Build for version $1
+
+ -- Linus Fischer <leicraft@leicraftmc.de>  $(date -R)
+
+EOF
+
+# Build
+INSERT_LEIOS_RELEASE=$(echo $1) dpkg-buildpackage -us -uc
+
+# Cleanup
+rm -f debian/changelog
